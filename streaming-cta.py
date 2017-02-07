ea1a91aee41ea9709e94944fb37bf13cc0257b4b
Kai Brügge <kai.bruegge@tu-dortmund.de>
2017-02-07 13:39:24 +0100

    make a small script out of the screaming business

--- streaming-cta.py
+++ streaming-cta.py
@@ -1,92 +1,29 @@
-from ctapipe.image.cleaning import tailcuts_clean
-from ctapipe.image.hillas import hillas_parameters
-from ctapipe.reco.FitGammaHillas import FitGammaHillas
-import numpy as np
 from queue import Queue
-from astropy import units as u
 import time
-import os
-import gzip
-import pickle
-import os.path
 import logging
-from itertools import cycle
-from ctapipe.io.containers import InstrumentContainer
+from cta import load_cam_geoms, load_instrument, load_event_generator
+import cta.analysis
+import click
 
 
 def batch_reco(dicts, instrument_dict):
-    return [reco(e, instrument_dict) for e in dicts]
-
-
-def reco(hillas_dict, instrument_dict):
-    instrument = InstrumentContainer()
-    instrument.__dict__ = instrument_dict
-
-    logging.info('starting reco for dict: {}'.format(hillas_dict))
-    tel_phi = {tel_id: 0*u.deg for tel_id in hillas_dict.keys()}
-    tel_theta = {tel_id: 20*u.deg for tel_id in hillas_dict.keys()}
-    fit_result = FitGammaHillas().predict(
-                            hillas_dict,
-                            instrument,
-                            tel_phi,
-                            tel_theta)
-
-    logging.info('Finished reco')
-    return fit_result
+    return [cta.analysis.reco(e, instrument_dict) for e in dicts]
 
 
 def batch_hillas(events, geoms):
-    return [hillas(e, geoms) for e in events]
+    return [cta.analysis.hillas(e, geoms) for e in events]
 
 
-def hillas(event, geoms):
-    hillas_dict = {}
-    for tel_id in event['data']:
-        pmt_signal = np.array(event['data'][tel_id]['adc_sums'])
-        cam_geom = geoms[tel_id]
-        mask = tailcuts_clean(cam_geom, pmt_signal, 1,
-                              picture_thresh=10., boundary_thresh=5.)
-        pmt_signal[mask == 0] = 0
-
-        moments = hillas_parameters(cam_geom.pix_x,
-                                    cam_geom.pix_y,
-                                    pmt_signal)
-
-        hillas_dict[tel_id] = moments
-
-    return hillas_dict
-
-
-def load_event_generator(working_dir):
-    p = os.path.join(working_dir, 'resources', 'gammas.pickle.gz')
-    with gzip.open(p, 'rb') as f:
-        event_list = pickle.load(f)
-
-    events = []
-    for e in event_list:
-        d = {
-             'event_id': np.asscalar(e.dl0.event_id),
-             'run_id': e.dl0.run_id,
-             'data': {},
-             }
-
-        for tel_id, t in e.dl0.tel.items():
-            d['data'][tel_id] = {'adc_sums': t.adc_sums[0].tolist()}
-
-        events.append(d)
-    return cycle(events)
-
-
-def load_data(q, events, batch_size=5):
+def load_data(q, events, batch_size=100, sleep=2):
     while True:
         q.put([next(events) for k in range(batch_size)])
-        time.sleep(0.12)
+        time.sleep(sleep)
 
 
 def monitor_q(q, name='result queue'):
     while True:
         print('Items currently in {} : {}'.format(name, q.qsize()))
-        time.sleep(1)
+        time.sleep(5)
 
 
 def get_results(q):
@@ -96,35 +33,27 @@ def get_results(q):
 
         if results:
             r = len(results)
-            l = len(results[0])
+            b = len(results[0])
+            e = len(results[0][0])
             dt = time.time() - s
             s = time.time()
-            print('Latest results: {} elements of length {} in {:.1f} seconds \n'
-                  'thats {:.2f} elements per second'.format(r, l, dt,  r*l/dt))
-
-        else:
-            print('no results')
+            print('Got {} results of {} batches containing {} events in {:.1f} seconds \n'
+                  'thats {:.2f} elements per second'.format(r, b, e, dt,  (r*b*e)/dt))
         time.sleep(5)
 
 
-def main():
-
-    def load_instrument(DIR):
-        p = os.path.join(DIR, 'resources', 'instrument.pickle.gz')
-        with gzip.open(p, 'rb') as f:
-            return pickle.load(f)
-
-    def load_cam_geoms(DIR):
-        p = os.path.join(DIR, 'resources', 'geoms.pickle.gz')
-        with gzip.open(p, 'rb') as f:
-            return pickle.load(f)
+@click.command()
+@click.option('--batch_size', '-b', default=1, help='Number of events in one batch.')
+@click.option('--input_q_size', '-qs', default=10, help='Number of events to hold in the input queue.')
+@click.option('--sleep', '-s', default=0.01, help='Delay until new batch is pushed into queue.')
+def main(batch_size, input_q_size, sleep):
 
     from distributed import Client
     client = Client('127.0.0.1:8786')
 
-    generator = load_event_generator('./')
-    instrument = load_instrument('./').as_dict()
-    geoms = load_cam_geoms('./')
+    generator = load_event_generator()
+    instrument = load_instrument().as_dict()
+    geoms = load_cam_geoms()
 
     logging.info('loaded instrument and geometries, distributing to workers')
     instrument_remote = client.scatter(instrument, broadcast=True)
@@ -133,7 +62,7 @@ def main():
     # hillas = partial(hillas, geoms=geometries_remote)
     # reco = partial(reco, instrument=instrument_remote)
 
-    input_q = Queue(maxsize=200)
+    input_q = Queue(maxsize=input_q_size)
     remote_queue = client.scatter(input_q)
 
     hillas_q = client.map(batch_hillas, remote_queue, **{'geoms': geometries_remote})
@@ -142,12 +71,11 @@ def main():
     result_q = client.gather(reco_q)
 
     from threading import Thread
-    Thread(target=load_data, args=(input_q, generator, 40), daemon=True).start()
+    Thread(target=load_data, args=(input_q, generator, batch_size, sleep), daemon=True).start()
 
+    # Thread(target=load_data, args=(input_q, generator, 90), daemon=True).start()
 
-    #Thread(target=load_data, args=(input_q, generator, 90), daemon=True).start()
-
-    #Thread(target=load_data, args=(input_q, generator, 60), daemon=True).start()
+    # Thread(target=load_data, args=(input_q, generator, 60), daemon=True).start()
     Thread(target=monitor_q, args=(input_q, 'input queue'), daemon=True).start()
 
     get_results(result_q)
