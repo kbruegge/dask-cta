de12a240d9e076a33c53ba65ddca5694c85b4aaf
Kai Brügge <kai.bruegge@tu-dortmund.de>
2017-04-27 11:53:45 +0200

    add changes to the local benchmark from ages ago

--- local_benchmark.py
+++ local_benchmark.py
@@ -2,16 +2,22 @@ from daskcta import load_cam_geoms, load_instrument, load_event_generator
 import daskcta.analysis as analysis
 
 
+generator = load_event_generator()
+instrument = load_instrument().as_dict()
+geoms = load_cam_geoms()
+
+
+def calc():
+    event = next(generator)
+    hillas_dict = analysis.hillas(event, geoms)
+    return analysis.reco(hillas_dict, instrument)
+
+
 def main():
-    generator = load_event_generator()
-    instrument = load_instrument().as_dict()
-    geoms = load_cam_geoms()
     r = []
 
     for _ in range(25):
-        event = next(generator)
-        hillas_dict = analysis.hillas(event, geoms)
-        r.append(analysis.reco(hillas_dict, instrument))
+        r.append(calc())
 
     print('Finished analysing {} events'.format(len(r)))
 
